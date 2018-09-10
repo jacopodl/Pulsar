37427dcc9ed9e93ae328734d52416e0d42935b44
Jacopo De Luca <jacopo.delu@gmail.com>
2018-09-10 16:36:14 +0200

    improvements for cipher :D

--- src/handle/cipher.go
+++ src/handle/cipher.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/des"
+	"encoding/binary"
 	"fmt"
 	"math/rand"
 	"strings"
@@ -12,11 +13,13 @@ import (
 
 const (
 	DEFAULTCIPHER = "aes"
-	VERIFYBLOCK   = 6
+	VERIFYBLOCK   = 4
+	HEADERSIZE    = 2
 )
 
 type cipherSt struct {
-	block cipher.Block
+	block  cipher.Block
+	buffer []byte
 }
 
 func NewCipher() Handler {
@@ -55,41 +58,83 @@ func (c *cipherSt) Init(options string) (Handler, error) {
 }
 
 func (c *cipherSt) Process(buf []byte, length int, decode bool) ([]byte, int, error) {
+	var pdata []byte = nil
+	buf = buf[:length]
 
 	if !decode {
-		ciphertext, err := encryptCTR(buf[:length], c.block)
+		ciphertext, err := encryptCTR(buf, c.block)
 		return ciphertext, len(ciphertext), err
 	}
-	plaintext, err := decryptCTR(buf[:length], c.block)
-	return plaintext, len(plaintext), err
+
+	for {
+		ciphertext, haveNext, next := c.cipherChunk(buf)
+		if ciphertext == nil {
+			return pdata, len(pdata), nil
+		}
+
+		plaintext, err := decryptCTR(ciphertext, c.block)
+		if !haveNext && pdata == nil {
+			return plaintext, len(plaintext), err
+		}
+		pdata = append(pdata, plaintext...)
+		buf = buf[next:]
+	}
+}
+
+func (c *cipherSt) cipherChunk(data []byte) ([]byte, bool, int) {
+	if c.buffer != nil {
+		if len(c.buffer) == 1 {
+			c.buffer = append(c.buffer, data[0])
+			data = data[1:]
+		}
+		length := int(binary.BigEndian.Uint16(c.buffer[:HEADERSIZE]))
+		remain := int(length) - len(c.buffer) + HEADERSIZE
+		if len(data) < remain {
+			c.buffer = append(c.buffer, data...)
+			return nil, false, -1
+		}
+		c.buffer = append(c.buffer, data[:remain]...)
+		tmp := c.buffer
+		c.buffer = nil
+		return tmp, len(data) > remain, remain
+	}
+
+	if len(data) > 1 {
+		length := int(binary.BigEndian.Uint16(data[:HEADERSIZE]))
+		if len(data[HEADERSIZE:]) >= length {
+			return data[:HEADERSIZE+length], len(data[HEADERSIZE+length:]) > 0, HEADERSIZE + length
+		}
+	}
+	c.buffer = append(c.buffer, data...)
+	return nil, false, -1
 }
 
 func encryptCTR(data []byte, block cipher.Block) ([]byte, error) {
-	var vblock []byte = nil
+	var vblk []byte = nil
 	var err error = nil
 
-	ciphertext := make([]byte, block.BlockSize()+VERIFYBLOCK+len(data))
+	ciphertext := make([]byte, HEADERSIZE+block.BlockSize()+VERIFYBLOCK+len(data))
 
-	iv := ciphertext[:block.BlockSize()]
+	binary.BigEndian.PutUint16(ciphertext[:HEADERSIZE], uint16(len(data)+block.BlockSize())+VERIFYBLOCK)
 
-	rand.Seed(time.Now().UnixNano())
-	if _, err = rand.Read(iv); err != nil {
+	iv := ciphertext[HEADERSIZE : HEADERSIZE+block.BlockSize()]
+	if err = mkRandom(iv); err != nil {
 		return nil, err
 	}
 
-	if vblock, err = mkKeyVerBlock(data, byte(block.BlockSize())); err != nil {
+	if vblk, err = mkKeyVerBlock(data, byte(block.BlockSize())); err != nil {
 		return nil, err
 	}
 
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertext[block.BlockSize():], vblock)
+	stream.XORKeyStream(ciphertext[HEADERSIZE+block.BlockSize():], vblk)
 	return ciphertext, nil
 }
 
 func decryptCTR(data []byte, block cipher.Block) ([]byte, error) {
-	plaintext := make([]byte, len(data)-block.BlockSize())
-	stream := cipher.NewCTR(block, data[:block.BlockSize()])
-	stream.XORKeyStream(plaintext, data[block.BlockSize():])
+	plaintext := make([]byte, len(data)-block.BlockSize()-HEADERSIZE)
+	stream := cipher.NewCTR(block, data[HEADERSIZE:HEADERSIZE+block.BlockSize()])
+	stream.XORKeyStream(plaintext, data[HEADERSIZE+block.BlockSize():])
 
 	if !verifyKey(plaintext[:VERIFYBLOCK], byte(block.BlockSize())) {
 		return nil, fmt.Errorf("invalid algorithm or key")
@@ -98,31 +143,33 @@ func decryptCTR(data []byte, block cipher.Block) ([]byte, error) {
 	return plaintext[VERIFYBLOCK:], nil
 }
 
-func mkKeyVerBlock(data []byte, blocksize byte) ([]byte, error) {
-	block := make([]byte, VERIFYBLOCK)
+func mkRandom(data []byte) error {
 	rand.Seed(time.Now().UnixNano())
-	if _, err := rand.Read(block); err != nil {
+	_, err := rand.Read(data)
+	return err
+}
+
+func mkKeyVerBlock(data []byte, blksize byte) ([]byte, error) {
+	block := make([]byte, VERIFYBLOCK)
+	if err := mkRandom(data); err != nil {
 		return nil, err
 	}
 
-	//   |0|1|2|3|4|5|
-	// 1) ^-------^ <-----cipher block size
-	// 2)     ^-----^
-	// 3)   ^---^
+	//   |0|1|2|3|
+	// 1) ^---^ <-----cipher block size
+	// 2)   ^---^
 
 	// 1
-	block[0] = blocksize
-	block[4] = block[0]
+	block[0] = blksize
+	block[2] = blksize
 	// 2
-	block[2] = block[5]
-	// 3
 	block[1] = block[3]
 
 	return append(block, data...), nil
 }
 
-func verifyKey(block []byte, blocksize byte) bool {
-	return blocksize == block[0] && block[0] == block[4] && block[2] == block[5] && block[1] == block[3]
+func verifyKey(block []byte, blksize byte) bool {
+	return blksize == block[0] && block[0] == block[2] && block[1] == block[3]
 }
 
 func splitCipherOptions(options, defcipher string) (algo, key string) {
