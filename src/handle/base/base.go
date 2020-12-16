fa7d850b99e83339c13634644e0941687fd2e212
Jacopo De Luca <jacopodl@users.noreply.github.com>
2020-12-16 13:34:13 +0100

    removed dead code

--- src/handle/base/base.go
+++ src/handle/base/base.go
@@ -54,7 +54,6 @@ func SplitInputBuffer(buf []byte, baseChunk int, paddingRune byte) (int, bool) {
 	for i := baseChunk; i < len(buf); i += baseChunk {
 		if buf[i-1] == paddingRune {
 			return i, true
-			break
 		}
 	}
 	return len(buf), len(buf)%baseChunk == 0
